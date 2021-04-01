b021c01e9180283eb86eeb802c91c9e510fe6148
Gustavo Michels <gmichels@nanthealth.com>
2021-04-01 14:09:55 -0400

    Create node specific metrics dynamically; add some more logging

--- exporter.py
+++ exporter.py
@@ -15,13 +15,15 @@ GRID_NODE_COUNT = Gauge('grid_node_count', 'Number of nodes in grid')
 GRID_SESSION_COUNT = Gauge('grid_session_count', 'Number of running sessions')
 GRID_SESSION_QUEUE_SIZE = Gauge('grid_session_queue_size', 'Number of queued sessions')
 
+# supported browsers
+BROWSERS = ['chrome', 'firefox', 'edge', 'opera']
 # node metrics
-# chrome
-NODE_SLOT_COUNT_CHROME = Gauge('node_slot_count_chrome', 'Total number of Chrome slots')
-NODE_SESSION_COUNT_CHROME = Gauge('node_session_count_chrome', 'Number of running Chrome sessions')
-# firefox
-NODE_SLOT_COUNT_FIREFOX = Gauge('node_slot_count_firefox', 'Total number of Firefox slots')
-NODE_SESSION_COUNT_FIREFOX = Gauge('node_session_count_firefox', 'Number of running Firefox sessions')
+for entry in BROWSERS:
+    # dynamically create metric container for each browser
+    globals()[f'NODE_SLOT_COUNT_{entry.upper()}'] = Gauge(f'node_slot_count_{entry}',
+                                                          f'Total number of {entry.capitalize()} slots')
+    globals()[f'NODE_SESSION_COUNT_{entry.upper()}'] = Gauge(f'node_session_count_{entry}',
+                                                             f'Total number of {entry.capitalize()} slots')
 
 
 def process_metrics() -> None:
@@ -37,6 +39,8 @@ def process_metrics() -> None:
         generate_grid_metrics(resp_body['data']['grid'])
         # generate the node metrics
         generate_node_metrics(resp_body['data']['nodesInfo']['nodes'])
+    else:
+        logging.warning(f'Unable to retrieve metrics from the grid (response status code {resp.status_code})')
 
 
 def generate_grid_metrics(data: dict) -> None:
@@ -45,6 +49,7 @@ def generate_grid_metrics(data: dict) -> None:
 
     :param data: the grid data dict
     """
+    logging.debug('Publishing Grid metrics')
     GRID_TOTAL_SLOTS.set(data['totalSlots'])
     GRID_NODE_COUNT.set(data['nodeCount'])
     GRID_SESSION_COUNT.set(data['sessionCount'])
@@ -59,19 +64,21 @@ def generate_node_metrics(data: dict) -> None:
     """
     # initialize stats
     node_stats = {'slot_count': 0, 'session_count': 0}
-    all_nodes = {'chrome': node_stats.copy(), 'firefox': node_stats.copy()}
+    all_nodes = dict()
+    for browser in BROWSERS:
+        all_nodes.update({browser: node_stats.copy()})
+
     # generate the consolidated information about all nodes
     for node in data:
         browser = json.loads(node['stereotypes'])[0]['stereotype']['browserName']
         all_nodes[browser]['slot_count'] += node['slotCount']
         all_nodes[browser]['session_count'] += node['sessionCount']
 
-    # publish Chrome metrics
-    NODE_SLOT_COUNT_CHROME.set(all_nodes['chrome']['slot_count'])
-    NODE_SESSION_COUNT_CHROME.set(all_nodes['chrome']['session_count'])
-    # publish Firefox metrics
-    NODE_SLOT_COUNT_FIREFOX.set(all_nodes['firefox']['slot_count'])
-    NODE_SESSION_COUNT_FIREFOX.set(all_nodes['firefox']['session_count'])
+    # publish browser metrics
+    for browser in BROWSERS:
+        logging.debug(f'Publishing {browser.capitalize()} metrics')
+        globals()[f'NODE_SLOT_COUNT_{browser.upper()}'].set(all_nodes[browser]['slot_count'])
+        globals()[f'NODE_SESSION_COUNT_{browser.upper()}'].set(all_nodes[browser]['session_count'])
 
 
 if __name__ == '__main__':
